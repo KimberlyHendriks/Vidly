8aa8dd3fc8ea09718605943a491d0446fe6ac0a9
Kimberly Hendriks <kimberly.hendriks@live.nl>
2017-09-26 14:43:56 +0200

    Added Migrations

---
+++ Migrations/201709251701017_UpdateMembershipTypes.cs
@@ -0,0 +1,23 @@
+using System.Data.Entity.Core.Metadata.Edm;
+
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes SET Type = 'Pay as You Go' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET Type = 'Monthly' WHERE Id = 2");
+            Sql("UPDATE MembershipTypes SET Type = 'Quarterly' WHERE Id = 3");
+            Sql("UPDATE MembershipTypes SET Type = 'Annual' WHERE Id = 4");
+            
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
